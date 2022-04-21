020b348e1a48b42c60cdb914ceb24072b431d8cc
okkevaneck <okke.van.eck@gmail.com>
2022-04-21 14:37:17 +0200

    Added scripts for intel advisor

---
+++ intel_advisor/setup_env_das.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Used links:
+#   Setup:  https://www.intel.com/content/www/us/en/develop/documentation/advisor-user-guide/top/install-and-launch/set-up-environment-variables.html#set-up-environment-variables
+#   Launch: https://www.intel.com/content/www/us/en/develop/documentation/advisor-user-guide/top/install-and-launch/launch-gui.html
+
+# Setup variables for using CLI.
+source intel/oneapi/setvars.sh
+export ADVISOR_2021_DIR="intel/oneapi/advisor/latest"
