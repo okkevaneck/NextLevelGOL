f620c68752a85d29a32c557fd2289b07f7c85de9
okkevaneck <okke.van.eck@gmail.com>
2022-05-11 17:10:13 +0200

    Added throughput plot

--- profiler/generate_figures.py
+++ profiler/generate_figures.py
@@ -9,36 +9,33 @@ import os
 import glob
 
 
-def load_results(v):
-    fs = glob.glob(f"results/{results_folder}/{v}_*")
-    with open(fs[0], "r") as fp:
-        values = {}
-        lines = fp.readlines()
-        values["wrap"] = float(lines[1][11:16])
-        values["step"] = float(lines[2][11:16])
-        values["swap"] = float(lines[3][11:16])
-        values["gif"] = float(lines[4][11:16])
-
-    return values
-
-
-def gen_barplot():
-    # Arrays with normalized values.
+def load_results():
     norm_values = {
         "wraps": [],
         "steps": [],
         "swaps": [],
         "gifs": [],
+        "throughput": [],
     }
 
     # Load results in variables.
-    timings = {}
     for v in versions:
-        timings[v] = load_results(v)
-        norm_values["wraps"].append(timings[v]["wrap"])
-        norm_values["steps"].append(timings[v]["step"])
-        norm_values["swaps"].append(timings[v]["swap"])
-        norm_values["gifs"].append(timings[v]["gif"])
+        fs = glob.glob(f"results/{results_folder}/{v}_*")
+        with open(fs[0], "r") as fp:
+            lines = fp.readlines()
+
+            norm_values["wraps"].append(float(lines[1][11:16]))
+            norm_values["steps"].append(float(lines[2][11:16]))
+            norm_values["swaps"].append(float(lines[3][11:16]))
+            norm_values["gifs"].append(float(lines[4][11:16]))
+            norm_values["throughput"].append(float(lines[8][12:18]))
+
+    return norm_values
+
+
+def gen_barplot():
+    # Arrays with measured values.
+    norm_values = load_results()
 
     # Normalize the arrays.
     metrics = ["wraps", "steps", "swaps", "gifs"]
@@ -74,6 +71,45 @@ def gen_barplot():
     plt.show()
 
 
+def gen_throughput():
+    # Arrays with measured values.
+    norm_values = load_results()
+
+    # Create DataFrame with normalized performance numbers.
+    df = pd.DataFrame({"Version": versions,
+                       "throughput": norm_values["throughput"]})
+
+    sns.set(style="white")
+    ax = sns.barplot(x="Version", y="throughput", data=df, color="#4878D0")
+
+    # Compose values per 1000.
+    bar_values = []
+    for rec in ax.containers[0]:
+        txt = str(round(rec.get_height(), -3))[:-5] + "K"
+        ax.text(x=rec.get_x() + rec.get_width() / 2, y=rec.get_height()+.5,
+                s=txt, ha="center")
+
+    # Add info to plot.
+    plt.title("Throughput per version", fontsize=16)
+    plt.xlabel("Versions", labelpad=0)
+    plt.ylabel("Throughput (pixels/second)")
+    plt.tight_layout()
+    plt.xticks(rotation=45)
+    plt_ax = plt.gca()
+    plt_ax.tick_params(axis="both", which="major", pad=0)
+
+    # Compose ylabel per 1000.
+    ylbls = ax.get_yticklabels()
+    new_labels = []
+    for ylbl in ylbls:
+        new_labels.append(str(round(float(ylbl.get_text()), -3))[:-5] + "K")
+    ax.set_yticklabels(new_labels)
+
+    # Save and show plot.
+    plt.savefig(f"figures/{results_folder}_throughput_{'-'.join(versions)}")
+    plt.show()
+
+
 if __name__ == "__main__":
     # Get results folder and versions from command line.
     args = sys.argv
@@ -92,5 +128,8 @@ if __name__ == "__main__":
             print(f"Results for version '{ver}' do not exist..")
             exit(1)
 
-    # Generate normalized bar plot with the given versions.
-    gen_barplot()
+    # # Generate normalized bar plot with the given versions.
+    # gen_barplot()
+
+    # Generate throughput plot with the given versions.
+    gen_throughput()
