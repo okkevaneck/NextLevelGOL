1e0376df8c3afe979fa096ef4e93ad6df0097304
okkevaneck <okke.van.eck@gmail.com>
2022-05-12 11:55:41 +0200

    Added gif for clean results

--- intel_advisor/clean_results.sh
+++ intel_advisor/clean_results.sh
@@ -2,4 +2,4 @@
 # Abort if script is not run from a version.
 # TODO
 
-rm -rf annotations.advidb2 e000/ config/ roofline-withstack.html  *.advixeproj
+rm -rf annotations.advidb2 e000/ config/ roofline-withstack.html  *.advixeproj out.gif
