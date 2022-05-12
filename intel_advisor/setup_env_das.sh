3c85b3abe93efc622bd0e3d14d33f859ebe1794f
Okke van Eck <okke.van.eck@gmail.com>
2022-05-12 11:34:33 +0200

    Changed setup env das

--- intel_advisor/setup_env_das.sh
+++ intel_advisor/setup_env_das.sh
@@ -4,5 +4,5 @@
 #   Launch: https://www.intel.com/content/www/us/en/develop/documentation/advisor-user-guide/top/install-and-launch/launch-gui.html
 
 # Setup variables for using CLI.
-source intel/oneapi/setvars.sh
-export ADVISOR_2021_DIR="intel/oneapi/advisor/latest"
+source $HOME/intel/oneapi/setvars.sh
+export ADVISOR_2021_DIR="$HOME/intel/oneapi/advisor/latest"
