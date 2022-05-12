aa408c11c957cac6ca31eb62f057661b151c969d
okkevaneck <okke.van.eck@gmail.com>
2022-05-12 13:23:52 +0200

    Added og ref and figured that run_advisor.sh already exists

--- intel_advisor/run_advisor.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Abort if script is not run from a version.
-# TODO
-
-# Collect data from version in current directory.
-prun -np 1 advisor --collect=roofline --project-dir=. -- ./gol 1000 1000 1000 -o /dev/null
-
-# Generate report from collected data in current directory.
-prun -np 1 advisor --report=roofline --with-stack --project-dir=. --report-output=./roofline-withstack.html
