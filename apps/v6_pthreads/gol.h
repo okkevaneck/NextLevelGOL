145d84d46854b005c1ab3c8fce142a57f02e352c
Kevin Nobel <kevin@2sk.nl>
2022-05-31 15:29:32 +0200

    Fresh start for pthreads

--- apps/v6_pthreads/gol.h
+++ apps/v6_pthreads/gol.h
@@ -1,17 +1,1 @@
-#ifndef GOL_H
-#define GOL_H
-
-#include <inttypes.h>
-#include "pthread.h"
-
-typedef uint8_t pixel_t;
-
-typedef struct {
-    int width, height;
-    pixel_t **cells;
-} world;
-
-void world_border_timestep(world *old, world *new);
-void world_timestep(world *old, world *new, int start_row, int end_row);
-
-#endif
+../v2_updated_border_code/gol.h
