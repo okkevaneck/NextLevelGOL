6d1adb4f0a53032ea81c435210ff4e5aa711123f
okkevaneck <okke.van.eck@gmail.com>
2022-06-01 16:38:13 +0200

    Midway barplot for result merge

--- profiler/generate_barplot.py
+++ profiler/generate_barplot.py
@@ -25,19 +25,8 @@ def load_results():
                 rows.append({"version": v, "type": "swap", "value": float(lines[4][12:17])})
                 rows.append({"version": v, "type": "gif", "value": float(lines[5][12:17])})
                 rows.append({"version": v, "type": "final", "value": float(lines[6][12:17])})
-                # rows.append({"version": v, "type": "total", "value": float(lines[8][11:16])})
-                # rows.append({"version": v, "type": "throughput", "value": float(lines[10][12:18])})
-
-                # value = {"version": v,
-                #          "init": float(lines[1][12:17]),
-                #          "wrap": float(lines[2][12:17]),
-                #          "step": float(lines[3][12:17]),
-                #          "swap": float(lines[4][12:17]),
-                #          "gif": float(lines[5][12:17]),
-                #          "final": float(lines[6][12:17]),
-                #          "total": float(lines[8][11:16]),
-                #          "throughput": float(lines[10][12:18])}
-                # rows.append(value)
+                rows.append({"version": v, "type": "total", "value": float(lines[8][11:16])})
+                rows.append({"version": v, "type": "throughput", "value": float(lines[10][12:18])})
 
     values = pd.DataFrame(rows)
 
@@ -45,20 +34,23 @@ def load_results():
 
 
 def gen_barplot():
-    # Arrays with measured values.
+    # Fetch DatFrame with measured values.
     df = load_results()
-
-    # Make normalized DataFrame.
-    # df = values.iloc[:, 1:7] = values.iloc[:, 1:7].div(values.iloc[:, 1:7].sum(axis=1), axis=0)
     print(df)
 
-    # Create DataFrame with mean values.
+    # Create DataFrame with mean values and normalize.
     df_mean = df.pivot_table(index="version",
-                            columns="type",
-                            values="value",
-                            aggfunc="mean")
+                             columns="type",
+                             values="value",
+                             aggfunc="mean")
+    # Order DataFrame to plot in order.
+    cols = ["init", "wrap", "step", "swap", "gif", "final", "total", "throughput"]
+    df_mean = df_mean[cols]
 
     print(df_mean)
+    df_mean_norm = df_mean.copy()
+    df_mean_norm.iloc[:, 0:6] = df_mean_norm.iloc[:, 0:6].div(df_mean_norm.iloc[:, 0:6].sum(axis=1), axis=0)
+    print(df_mean_norm)
 
     # Create DataFrame for the error bars (std).
     df_std = df.pivot_table(index="version",
@@ -67,8 +59,16 @@ def gen_barplot():
                             aggfunc="std")
     print(df_std)
 
-    # plot the dataframe and add yerr
-    ax = df_mean.plot(kind="bar", stacked=True, figsize=(9, 6), rot=0, yerr=df_std)
+    # Scale the standard deviation to be relative for the 0-1 scale.
+    # print(df_std[cols[:-2]])
+    for i, row in enumerate(df_std[cols[:-2]].iterrows()):
+        print(f"{row}\n")
+        row /= df_mean.iloc[i]["total"]
+        df_std[cols[:-2]] = row
+    print(df_std)
+
+    # Plot the dataframe and add yerr.
+    ax = df_mean_norm[cols[:-2]].plot(kind="bar", stacked=True, figsize=(9, 6), rot=0, yerr=df_std)
     plt.show()
 
     # TODO: https://stackoverflow.com/questions/70333645/how-to-annotate-bar-plots-when-adding-error-bars
